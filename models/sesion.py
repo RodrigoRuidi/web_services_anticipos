4d42db650997500a885c62d4ae64e7931baeda32
oxalcvasquez <oxalc0101@gmail.com>
2022-10-28 17:23:24 -0500

    Services and model of login

---
+++ models/sesion.py
@@ -0,0 +1,81 @@
+from conexionBD import Conexion as bd
+import json
+from util import CustomJsonEncoder
+
+class Sesion():
+    def __init__(self,email=None,password=None):
+      self.email = email
+      self.password = password
+
+    def iniciar_sesion(self):
+      #Open database
+      con = bd().open
+      #Creation cursor
+      cursor = con.cursor()
+      #Query sql
+      sql = 'SELECT id,nombres,apellido,email,estado_usuario,rol_id FROM usuario WHERE email=%s and password=%s'
+      #Execute SQL query
+      cursor.execute(sql,[self.email,self.password])
+      #Get data from cursor
+      datos = cursor.fetchone()
+      #Close the cursor
+      cursor.close()
+      #Clse connection
+      con.close()
+
+      if datos:
+        if datos['estado_usuario'] == '1':
+          return json.dumps({'status': True,'data': datos,'message':'Credenciales correcta'})
+        else :
+          return json.dumps({'status':False,'data':'','message':'El usuario se encuentra inactivo'})
+      else :
+        return json.dumps({'status' : False, 'data':'','message':'Credenciales incorrectas o el usuario no existe'})
+
+    def actualizar_token(self,token,user_id):
+      #Open connection
+      con = bd().open
+      #Create cursor
+      cursor = con.cursor()
+      #Prepare query - Update data
+      sql = "UPDATE usuario SET token = %s, estado_token = '1', WHERE id = %s"
+
+      try:
+        cursor.execute(sql[token,user_id])
+        con.commit() #Save
+        return json.dumps({'status':True,'message':'Token actualizado'})
+      except con.Error as error:
+        print(error)
+        con.rollback()
+        return json.dumps({'status':False,'message':format(error)} , cls = CustomJsonEncoder)
+      finally:
+        cursor.close()
+        con.close()
+
+    def validar_estado_token(self,user_id):
+      #Open connection
+      con = bd().open
+      #Create cursor
+      cursor = con.cursor()
+      #Prepare query - Update data
+      sql = "SELECT estado_token FROM usuario WHERE id = %s"
+
+      #Execute query
+      cursor.execute(sql[user_id])
+
+      datos = cursor.fetchone()
+
+      cursor.close()
+      con.close()
+
+      if datos:
+        return json.dumps({'status':True,'data':datos,'message':'Estado del token'})
+      else :
+        return json.dumps({'status': True, 'data': '', 'message': 'Estado del token'})
+
+
+
+
+
+
+
+
---
+++ services/sesion.py
@@ -0,0 +1,38 @@
+#Web services login
+from flask import Blueprint,request,jsonify
+from models.sesion import Sesion
+from config import SecretKey
+import jwt
+import datetime
+import json
+
+#Interface for web services managment
+ws_sesion = Blueprint('ws_sesion',__name__)
+
+@ws_sesion.route('/login',methods=['POST'])
+
+def login():
+    if (request.method == 'POST'):
+      #Write data by POST
+      email = request.form['email']
+      password = request.form['password']
+
+      #Instance a sesion object
+      obj_sesion = Sesion(email,password)
+      #Execute login method
+      rpta_inicio_sesion = obj_sesion.iniciar_sesion()
+      #Convert the response in JSON String to JSON Object String
+      datos_sesion_JSON = json.loads(rpta_inicio_sesion)
+
+      if datos_sesion_JSON['status'] == True:
+        #Save user id in the token
+        user_id = datos_sesion_JSON['data']['id']
+        #Generate token
+        token = jwt.encode({'user_id':user_id,'exp': datetime.datetime.utcnow()+datetime.timedelta(seconds=60)},SecretKey.JWT_SECRET_JEY)
+        #Update generated token in database
+        obj_sesion.actualizar_token(token,user_id)
+
+        #Show web services response
+        return jsonify(datos_sesion_JSON), 200 #OK
+      else:
+        return jsonify(datos_sesion_JSON), 401  # Not Authorized
